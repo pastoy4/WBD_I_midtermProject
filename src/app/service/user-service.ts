1f6c61ca7753ad902f7efeef90e3a58f971f5db2
pastoy4 <kopkopgaming855@gmail.com>
2025-11-05 14:10:54 +0700

    ww

--- src/app/service/user-service.ts
+++ src/app/service/user-service.ts
@@ -1,30 +1,44 @@
 import { Injectable } from '@angular/core';
-declare const axios: any;
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() {
-    let ng_this = this;
+  
+  private apiUrl = 'http://localhost:9000/api/users'; // Base API URL
+  private uploadUrl = 'http://localhost:9000/api/upload/image';
 
-    axios.get('https://fakestoreapi.com/users')
-      .then(function (response: any) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error: any) {
-        // handle error
-        console.log(error);
-      })
+  constructor(private http: HttpClient){}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
   }
 
-  private users: any[] = [];
-
-  getUsers(): any[] {
-    return this.users;
+  getUserById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 
+  // <--- UPDATED ENDPOINT FOR REGISTRATION/CREATION --->
+  createUser(data: any): Observable<any> {
+    // Using the correct registration endpoint from user.js: POST /api/users/register
+    return this.http.post<any>(`${this.apiUrl}/register`, data);
+  }
+  // <--- END UPDATED ENDPOINT --->
 
+  updateUser(id: string, data: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, data);
+  }
+
+  deleteUser(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+
+  uploadImage(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('image', file, file.name);
+    return this.http.post<any>(this.uploadUrl, formData);
+  }
 }
--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,11 +1,91 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserService } from '../service/user-service';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './users.html',
   styleUrl: './users.css'
 })
-export class Users {
+export class Users implements OnInit {
+  users: any[] = [];
+  // Initialize the user object with necessary fields, including image
+  user: any = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: '',
+    password: '',
+    confirmPassword: '', 
+    gender: '',
+    dob: '',
+    image: '', 
+    roleName: '' 
+  };
+  isEdit = false;
+  editId: string = '';
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe((res: any) => {
+      // Assuming 'res' is an array of users
+      this.users = res;
+    });
+  }
+
+  saveUser() {
+    if (this.isEdit) {
+      // NOTE: Update logic for PUT is separate and might not require confirmPassword
+      this.userService.updateUser(this.editId, this.user).subscribe(() => {
+        this.getUsers();
+        this.resetForm();
+      });
+    } else {
+      // Calls the corrected POST /api/users/register endpoint
+      this.userService.createUser(this.user).subscribe(() => {
+        this.getUsers();
+        this.resetForm();
+      });
+    }
+  }
+
+  editUser(p: any) {
+    // Spread operator copies all properties from the user object 'p'
+    this.user = { ...p, confirmPassword: p.password }; // Add confirmPassword for edit modal
+    this.editId = p._id;
+    this.isEdit = true;
+  }
+
+  deleteUser(id: string) {
+    if (confirm('Are you sure to delete this user?')) {
+      this.userService.deleteUser(id).subscribe(() => {
+        this.getUsers();
+      });
+    }
+  }
+
+  resetForm() {
+    // Reset to the initial state
+    this.user = {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      gender: '',
+      dob: '',
+      image: '',
+      roleName: ''
+    };
+    this.isEdit = false;
+  }
 }
